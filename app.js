9827f3b11a7f69c790935d6750d27e0f0add49fe
cjallen216 <76927855+cjallen216@users.noreply.github.com>
2024-12-26 23:54:28 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -14,18 +14,6 @@ textLoad();
 
 setInterval(textLoad, 12000);
 
-const buttons = document.querySelectorAll("button");
-
-buttons.forEach((button) => {
-  button.addEventListener("click", () => {
-    const faq = button.nextElementSibling;
-    const icon = button.children[1];
-
-    faq.classList.toggle("show");
-    icon.classList.toggle("rotate");
-  });
-});
-
 let navigation = document.querySelector(".navigation");
 document.querySelector(".menu-toggle").onclick = function () {
   this.classList.toggle("active");
